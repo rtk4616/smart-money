7bf496d4a0d45ec099eaa489ff50b962cb7b63b6
weiyi.li <weiyi.just2@gmail.com>
2017-07-03 21:52:16 +0800

    [ Transaction List ] Use ConstraintLayout to define transaction list item.

--- app/src/main/java/me/li2/android/fiserv/smartmoney/ui/TransactionListAdapter.java
+++ app/src/main/java/me/li2/android/fiserv/smartmoney/ui/TransactionListAdapter.java
@@ -22,7 +22,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.h6ah4i.android.widget.advrecyclerview.swipeable.SwipeableItemAdapter;
@@ -67,15 +66,15 @@ public class TransactionListAdapter
     }
 
     public static class MyViewHolder extends AbstractSwipeableItemViewHolder {
-        public FrameLayout mContainer;
+        public ViewGroup mContainer;
         public TextView mTextView;
         public Button mButton;
 
         public MyViewHolder(View v) {
             super(v);
-            mContainer = (FrameLayout) v.findViewById(R.id.container);
-            mTextView = (TextView) v.findViewById(android.R.id.text1);
-            mButton = (Button) v.findViewById(android.R.id.button1);
+            mContainer = (ViewGroup) v.findViewById(R.id.transaction_item_container_view);
+            mTextView = (TextView) v.findViewById(R.id.transaction_date_view);
+            mButton = (Button) v.findViewById(R.id.transaction_detail_btn);
         }
 
         @Override
@@ -132,7 +131,7 @@ public class TransactionListAdapter
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        final View v = inflater.inflate(R.layout.list_item_with_leave_behind_button, parent, false);
+        final View v = inflater.inflate(R.layout.list_transaction_item, parent, false);
         return new MyViewHolder(v);
     }
 
